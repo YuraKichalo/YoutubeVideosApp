753d69818e8375905acc57c2136a7270773b2a63
Yurii Kichalo <yuriykichalo@gmail.com>
2019-09-20 17:51:56 +0300

    Updates

---
+++ precache-manifest.cacf134979293ab60fc92c435b603366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cb553f7ba5fc8bd4b985f424704b09",
+    "url": "/YoutubeVideosApp/index.html"
+  },
+  {
+    "revision": "c54bf30ca72fb0df1b3f",
+    "url": "/YoutubeVideosApp/static/css/main.904cabae.chunk.css"
+  },
+  {
+    "revision": "e30322f1d3e7afc7bae5",
+    "url": "/YoutubeVideosApp/static/js/2.541b4e13.chunk.js"
+  },
+  {
+    "revision": "c54bf30ca72fb0df1b3f",
+    "url": "/YoutubeVideosApp/static/js/main.ea2c957e.chunk.js"
+  },
+  {
+    "revision": "321a804277dd82a1f00a",
+    "url": "/YoutubeVideosApp/static/js/runtime~main.33090fed.js"
+  }
+]);
